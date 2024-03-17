64bb397cb33c54d15cf6176f07b07c64ad5fd0b2
vvhanxing <33742233+vvhanxing@users.noreply.github.com>
2024-03-18 01:41:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def upload_file():
     return jsonify({'message': caption,'fileName':file.filename}), 200
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0",port=5001,debug=True)
+    app.run(host = "0.0.0.0",port=5001,debug=True)
